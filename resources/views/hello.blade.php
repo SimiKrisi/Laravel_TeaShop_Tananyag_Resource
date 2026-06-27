b8758d74167b692ec9ae268b112b15ea4e7e835c
Simák Kristóf <simak.kristof2005@tanulo.boronkay.hu>
2026-06-27 13:20:47 +0200

    first section

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tea Shop</title>
+</head>
+<body>
+    <h1>Welcome to the Tea Shop</h1>
+    <p>Discover a variety of teas from around the world.</p>
+</body>
+</html>
